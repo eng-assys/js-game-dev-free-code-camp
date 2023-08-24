745505753bd7f671346a81780bd4178e20fda7cd
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-24 11:47:59 -0300

    testing moving square top down on canvas

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+let x = 0;
+let y = 50;
+
+function animate() {
+  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  context.fillRect(x, y, 100, 100);
+  x += 10;
+  if (x === 600) {
+    y += 10;
+    x = 0;
+  }
+
+  requestAnimationFrame(animate);
+}
+
+animate();
