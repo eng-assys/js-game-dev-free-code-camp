f5138140d8c0d0b3aa3dc677b55e0ff8a473fae6
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-25 10:05:23 -0300

    animate background layer without gap between two images; add game speed variation

--- 2-parallax-backgrounds-with-javascript/script.js
+++ 2-parallax-backgrounds-with-javascript/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
+const BACKGROUND_LAYER_SIZE = 2400; // in pixels
 let gameSpeed = 5;
 
 // Generate Layers Array
@@ -13,11 +14,17 @@ const backgroundLayer = [];
 })
 
 let x = 0;
+let x2 = BACKGROUND_LAYER_SIZE; // Size in pixels of background image
 
 function animate() {
   context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
   context.drawImage(backgroundLayer[3], x, 0);
-  x--;
+  context.drawImage(backgroundLayer[3], x2, 0);
+
+  x = x < -BACKGROUND_LAYER_SIZE ? BACKGROUND_LAYER_SIZE + x2 - gameSpeed : x - gameSpeed;
+  x2 = x2 < -BACKGROUND_LAYER_SIZE ? BACKGROUND_LAYER_SIZE + x - gameSpeed : x2 - gameSpeed;
+
   requestAnimationFrame(animate);
 }
 
