9a49f0e58981e8baad1b3893cd6695ed440eafe3
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-25 10:30:08 -0300

    add Layer class replacing previous animation code; draw different layer with parallax effect

--- 2-parallax-backgrounds-with-javascript/script.js
+++ 2-parallax-backgrounds-with-javascript/script.js
@@ -2,30 +2,60 @@ const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-const BACKGROUND_LAYER_SIZE = 2400; // in pixels
+const DEFAULT_LAYER_WIDTH = 2400;
+const DEFAULT_LAYER_HEIGHT = CANVAS_HEIGHT;
+const LAYERS_AMOUNT = 5;
 let gameSpeed = 5;
 
-// Generate Layers Array
-const backgroundLayer = [];
-['1', '2', '3', '4', '5'].map(layerNumber => {
-  let image = new Image();
-  image.src = `assets/layer-${layerNumber}.png`
-  backgroundLayer.push(image)
-})
+class Layer {
+  constructor(image, speedModifier) {
+    this.x = 0;
+    this.y = 0;
+    this.width = DEFAULT_LAYER_WIDTH;
+    this.height = DEFAULT_LAYER_HEIGHT;
+    this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = gameSpeed * speedModifier;
+  }
+  update() {
+    this.speed = gameSpeed * this.speedModifier;
+    if (this.x <= -this.width) {
+      this.x = this.width + this.x2 - this.speed;
+    }
+    if (this.x2 <= -this.width) {
+      this.x2 = this.width + this.x - this.speed;
+    }
+    this.x = Math.floor(this.x - this.speed);
+    this.x2 = Math.floor(this.x2 - this.speed);
+  }
+  draw() {
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    context.drawImage(this.image, this.x2, this.y, this.width, this.height);
+  }
+}
 
-let x = 0;
-let x2 = BACKGROUND_LAYER_SIZE; // Size in pixels of background image
+// Generate Layers Array
+const backgroundImages = [];
+for (let layerNumber = 0; layerNumber < LAYERS_AMOUNT; layerNumber++) {
+  let image = new Image();
+  image.src = `assets/layer-${layerNumber + 1}.png`
+  backgroundImages.push(image);
+}
+
+const backgroundLayers = [];
+backgroundImages.map((image, index) => {
+  let layer = new Layer(image, 0.5 * index);
+  backgroundLayers.push(layer);
+})
 
 function animate() {
   context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-  context.drawImage(backgroundLayer[3], x, 0);
-  context.drawImage(backgroundLayer[3], x2, 0);
-
-  x = x < -BACKGROUND_LAYER_SIZE ? BACKGROUND_LAYER_SIZE + x2 - gameSpeed : x - gameSpeed;
-  x2 = x2 < -BACKGROUND_LAYER_SIZE ? BACKGROUND_LAYER_SIZE + x - gameSpeed : x2 - gameSpeed;
-
   requestAnimationFrame(animate);
+  backgroundLayers.map(layer => {
+    layer.update();
+    layer.draw();
+  })
 }
 
 animate();
