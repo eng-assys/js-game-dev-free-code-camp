fa4e9b15b84c6801cb83733fdfc73c885dd449ce
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-25 13:35:11 -0300

    add list of 100 random enemies to canvas

--- 3-enemy-movement-patterns/script.js
+++ 3-enemy-movement-patterns/script.js
@@ -3,11 +3,13 @@ const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 100;
+const enemiesList = [];
 
 class Enemy {
   constructor() {
-    this.x = 10;
-    this.y = 50;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
     this.width = 100;
     this.height = 50;
   }
@@ -20,12 +22,16 @@ class Enemy {
   }
 }
 
-const enemy1 = new Enemy();
+for (let enemyIndex = 0; enemyIndex < numberOfEnemies; enemyIndex++) {
+  enemiesList.push(new Enemy());
+}
 
 function animate() {
   context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemy1.update();
-  enemy1.draw();
+  enemiesList.forEach(enemy => {
+    enemy.update();
+    enemy.draw();
+  });
   requestAnimationFrame(animate);
 }
 
