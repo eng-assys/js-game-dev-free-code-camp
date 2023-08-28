c6c4959f58a857b5e3a024e9eb99df55bda3c806
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-28 18:11:08 -0300

    create different emenies classes

--- 3-enemy-movement-patterns/script.js
+++ 3-enemy-movement-patterns/script.js
@@ -3,35 +3,56 @@ const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
-const ENEMIES_AMOUNT = 4;
-const numberOfEnemies = 100;
-const enemiesArray = [];
-
-const enemiesImages = [];
-for (let enemyNumber = 0; enemyNumber < ENEMIES_AMOUNT; enemyNumber++) {
-  let image = new Image();
-  image.src = `assets/enemy${enemyNumber + 1}.png`
-  enemiesImages.push(image);
-}
+const enemies = [
+  {
+    name: 'front_bats',
+    sprite: 'assets/enemy1.png',
+    amount: 20,
+    spriteWidth: 293,
+    spriteHeight: 155
+  },
+  {
+    name: 'horizontal_bats',
+    sprite: 'assets/enemy2.png',
+    amount: 10,
+    spriteWidth: 293,
+    spriteHeight: 155
+  },
+  {
+    name: 'ghosts',
+    sprite: 'assets/enemy3.png',
+    amount: 6,
+    spriteWidth: 293,
+    spriteHeight: 155
+  },
+  {
+    name: 'wheels',
+    sprite: 'assets/enemy4.png',
+    amount: 8,
+    spriteWidth: 293,
+    spriteHeight: 155
+  },
+];
+const enemiesArray = []
 
 let gameFrame = 0;
 
 class Enemy {
-  constructor() {
-    this.image = enemiesImages[0],
-      this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
+  constructor(image, spriteWidth, spriteHeight) {
+    this.image = new Image();
+    this.image.src = image;
+    this.spriteWidth = spriteWidth;
+    this.spriteHeight = spriteHeight;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x += Math.random() * 5 - 2.5;
+    this.y += Math.random() * 5 - 2.5;
     // animate sprites
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? this.frame = 0 : this.frame++;
@@ -52,11 +73,59 @@ class Enemy {
   }
 }
 
-
-
-for (let enemyIndex = 0; enemyIndex < numberOfEnemies; enemyIndex++) {
-  enemiesArray.push(new Enemy());
+class FrontBat extends Enemy {
+  constructor() {
+    super(
+      enemies[0].sprite,
+      enemies[0].spriteWidth,
+      enemies[0].spriteHeight
+    );
+  }
 }
+
+class HorizontalBat extends Enemy {
+  constructor() {
+    super(
+      enemies[1].sprite,
+      enemies[1].spriteWidth,
+      enemies[1].spriteHeight
+    );
+  }
+}
+
+class Ghost extends Enemy {
+  constructor() {
+    super(
+      enemies[2].sprite,
+      enemies[2].spriteWidth,
+      enemies[2].spriteHeight
+    );
+  }
+}
+
+class Wheel extends Enemy {
+  constructor() {
+    super(
+      enemies[3].sprite,
+      enemies[3].spriteWidth,
+      enemies[3].spriteHeight
+    );
+  }
+}
+
+const enemyClasses = {
+  front_bats: FrontBat,
+  horizontal_bats: HorizontalBat,
+  ghosts: Ghost,
+  wheels: Wheel
+}
+
+enemies.forEach(enemy => {
+  for (let i = 0; i < enemy.amount; i++) {
+    enemiesArray.push(new enemyClasses[enemy.name]());
+  }
+})
+
 
 function animate() {
   context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
