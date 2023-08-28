fe2fed7489279b5d8e80cef4af0cea08d2b6f39f
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-28 19:08:41 -0300

    add third enemy movement path

--- 3-enemy-movement-patterns/script.js
+++ 3-enemy-movement-patterns/script.js
@@ -23,7 +23,6 @@ class Enemy {
   }
   update() { }
   animateSprites(maxFrame) {
-    // animate sprites
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > maxFrame ? this.frame = 0 : this.frame++;
     }
@@ -73,7 +72,16 @@ class HorizontalBat extends Enemy {
 
 class Ghost extends Enemy {
   constructor() {
-    super('assets/enemy3.png', 293, 155);
+    super('assets/enemy3.png', 218, 188);
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 1.5 + 0.5;
+  }
+  update() {
+    this.x = canvas.width / 2 * Math.sin(this.angle * Math.PI / 90) + canvas.width / 2 - this.width / 2;
+    this.y = canvas.height / 2 * Math.sin(this.angle * Math.PI / 360) + canvas.height / 2 - this.height / 2;
+    this.angle += this.angleSpeed;
+    if (this.x + this.width < 0) this.x = canvas.width;
+    this.animateSprites(4);
   }
 }
 
@@ -90,11 +98,11 @@ const enemiesDefinitions = [
   },
   {
     class: HorizontalBat,
-    amount: 10
+    amount: 0
   },
   {
     class: Ghost,
-    amount: 0
+    amount: 50
   },
   {
     class: Wheel,
@@ -107,7 +115,6 @@ enemiesDefinitions.forEach(enemy => {
     enemiesArray.push(new enemy.class());
   }
 })
-
 
 function animate() {
   context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
