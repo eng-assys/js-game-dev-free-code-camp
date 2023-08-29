859a9023da2d6e694b503e4b91d826a0a8c0fd71
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-29 10:05:07 -0300

    add rotation to explosion animation

--- 4-collision-animations-from-a-sprite-sheet/script.js
+++ 4-collision-animations-from-a-sprite-sheet/script.js
@@ -12,36 +12,45 @@ class Explosion {
     this.spriteHeight = 179;
     this.width = this.spriteWidth / 2;
     this.height = this.spriteHeight / 2;
-    this.x = x - this.width / 2;
-    this.y = y - this.height / 2;
+    this.x = x;
+    this.y = y;
     this.image = new Image();
     this.image.src = 'boom.png';
     this.frame = 0;
     this.timer = 0;
+    this.angle = Math.random() * 6.2 // in radians - 360° is about 6.2 radians
   }
   update() {
     this.timer++;
     if (this.timer % 5 === 0) this.frame++;
   }
   draw() {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
     context.drawImage(
       this.image,
       this.spriteWidth * this.frame,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x,
-      this.y,
+      0 - this.width / 2,
+      0 - this.height / 2,
       this.width,
       this.height
-    )
+    );
+    context.restore();
   }
 }
 
-window.addEventListener('click', e => {
+function createAnimation(e) {
   let positionX = e.x - canvasPosition.left;
   let positionY = e.y - canvasPosition.top;
   explosionsArray.push(new Explosion(positionX, positionY));
+}
+
+window.addEventListener('click', e => {
+  createAnimation(e);
 });
 
 function animate() {
