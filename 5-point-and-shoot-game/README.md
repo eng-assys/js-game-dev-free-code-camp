41aaa2f87ccd3af6fe1a83bab331066509ce02de
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-29 20:56:57 -0300

    add project 5 bootstrap

---
+++ 5-point-and-shoot-game/README.md
@@ -0,0 +1,7 @@
+# Project5: Point and Shoot Game
+A Simple Implementation of Project 5: **Point and Shoot Game** from FreeCodeCamp's JavaScript Game Development Course for Beginners
+
+[Original Class](https://www.youtube.com/watch?v=GFO_txvwK_c&t=10115s)
+
+[Needed Sprites 1](https://www.frankslaboratory.co.uk/downloads/raven.png)
+[Needed Sprites 2](https://www.frankslaboratory.co.uk/downloads/boom.png)
---
+++ 5-point-and-shoot-game/script.js
@@ -0,0 +1,34 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let ravens = [];
+class Raven {
+  constructor() {
+    this.width = 100;
+    this.height = 50;
+    this.x = canvas.width;
+    this.y = Math.random() * (canvas.height - this.height);
+    this.directionX = Math.random() * 5 + 3;
+    this.directionY = Math.random() * 5 - 2.5;
+  }
+  update() {
+    this.x -= this.directionX;
+  }
+  draw() {
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+const raven = new Raven();
+
+function animate() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  console.log('test');
+  raven.update();
+  raven.draw();
+  requestAnimationFrame(animate);
+};
+animate();
