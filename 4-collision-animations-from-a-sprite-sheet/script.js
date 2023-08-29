6b95f01601488ae10b04cc1fc9d984ebceabb678
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-29 09:21:22 -0300

    show rectangle at mouse position on click

--- 4-collision-animations-from-a-sprite-sheet/script.js
+++ 4-collision-animations-from-a-sprite-sheet/script.js
@@ -3,6 +3,45 @@ const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 700;
+// const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
 
-context.fillStyle = 'white';
-context.fillRect(50, 50, 100, 150);
+class Explosion {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
+    this.image = new Image();
+    this.image.src = 'boom.png';
+    this.frame = 0;
+  }
+  update() {
+    this.frame++;
+  }
+  draw() {
+    context.drawImage(
+      this.image,
+      this.spriteWidth * this.frame,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    )
+  }
+}
+
+window.addEventListener('click', e => {
+  context.fillStyle = 'white';
+  context.fillRect(
+    e.x - canvasPosition.left,
+    e.y - canvasPosition.top,
+    50,
+    50
+  );
+});
