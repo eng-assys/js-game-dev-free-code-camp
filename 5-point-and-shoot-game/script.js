c996ae987380b2da38415ca549f7f2cfcd1f7a3e
Lucas Assis <lukas.ecomp@gmail.com>
2023-08-29 21:58:26 -0300

    add raven animation

--- 5-point-and-shoot-game/script.js
+++ 5-point-and-shoot-game/script.js
@@ -11,20 +11,38 @@ let lastTime = 0;
 let ravens = [];
 class Raven {
   constructor() {
-    this.width = 100;
-    this.height = 50;
+    this.image = new Image();
+    this.image.src = 'assets/raven.png';
+    this.spriteWidth = 271;
+    this.spriteHeight = 194;
+    this.sizeModifier = Math.random() * 0.6 + 0.4;
+    this.width = this.spriteWidth * this.sizeModifier;
+    this.height = this.spriteHeight * this.sizeModifier;
     this.x = canvas.width;
     this.y = Math.random() * (canvas.height - this.height);
     this.directionX = Math.random() * 5 + 3;
     this.directionY = Math.random() * 5 - 2.5;
     this.markedForDeletion = false;
+    this.frame = 0;
+    this.maxFrame = 5;
   }
   update() {
     this.x -= this.directionX;
     if (this.x < 0 - this.width) this.markedForDeletion = true;
+    this.frame = (this.frame >= this.maxFrame) ? 0 : this.frame + 1;
   }
   draw() {
-    context.fillRect(this.x, this.y, this.width, this.height);
+    context.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
