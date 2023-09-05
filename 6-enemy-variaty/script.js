03272d6da60cc2b945c52a7beb2cc3431709b701
Lucas Assis <lukas.ecomp@gmail.com>
2023-09-05 10:40:54 -0300

    add initial attributes to enemy class

--- 6-enemy-variaty/script.js
+++ 6-enemy-variaty/script.js
@@ -22,13 +22,16 @@ window.addEventListener('load', () => {
 
   class Enemy {
     constructor() {
-
+      this.x = 100;
+      this.y = 100;
+      this.width = 100;
+      this.height = 100;
     }
     update() {
-
+      this.x--;
     }
     draw() {
-
+      context.fillRect(this.x, this.y, this.width, this.height);
     }
   }
 
