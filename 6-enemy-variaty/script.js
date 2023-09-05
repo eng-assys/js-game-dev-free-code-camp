43bb72047a8ceaa14c6d977be0f51c7a4e847fde
Lucas Assis <lukas.ecomp@gmail.com>
2023-09-05 10:51:08 -0300

    add canvas property to game class; add game class to enemy class

--- 6-enemy-variaty/script.js
+++ 6-enemy-variaty/script.js
@@ -6,24 +6,30 @@ window.addEventListener('load', () => {
   canvas.height = 800;
 
   class Game {
-    constructor() {
-
+    constructor(context, width, height) {
+      this.context = context;
+      this.width = width;
+      this.height = height;
+      this.enemies = [];
+      this.#addNewEnemy();
+      console.log(this.enemies);
     }
     update() {
-
+      this.enemies.forEach(enemy => enemy.update());
     }
     draw() {
-
+      this.enemies.forEach(enemy => enemy.draw());
     }
     #addNewEnemy() {
-
+      this.enemies.push(new Enemy(this));
     }
   }
 
   class Enemy {
-    constructor() {
-      this.x = 100;
-      this.y = 100;
+    constructor(game) {
+      this.game = game;
+      this.x = this.game.width;
+      this.y = Math.random() * this.game.height;
       this.width = 100;
       this.height = 100;
     }
@@ -35,12 +41,14 @@ window.addEventListener('load', () => {
     }
   }
 
+  const game = new Game(context, canvas.width, canvas.height);
   let lastTime = 1;
   function animate(timeStamp) {
     context.clearRect(0, 0, canvas.width, canvas.height);
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp
-
+    game.update();
+    game.draw();
     requestAnimationFrame(animate);
   }
 
