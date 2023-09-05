e925c2f19b53208419f76c5af480fb34f4983824
Lucas Assis <lukas.ecomp@gmail.com>
2023-09-05 09:07:29 -0300

    add Game class tto controls everything in game

--- 6-enemy-variaty/script.js
+++ 6-enemy-variaty/script.js
@@ -1,4 +1,20 @@
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 800;
+
+class Game {
+  constructor() {
+
+  }
+  update() {
+
+  }
+  draw() {
+
+  }
+  #addNewEnemy() {
+
+  }
+}
