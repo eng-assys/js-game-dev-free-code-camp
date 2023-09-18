c64492aae18e39c5ee56c083d53ecfd73db689e8
Lucas Assis <lukas.ecomp@gmail.com>
2023-09-18 08:42:42 -0300

    fix collision detection to trigger when circles of collision touch each other

--- 7-side-scroller-game-with-mobile-support/script.js
+++ 7-side-scroller-game-with-mobile-support/script.js
@@ -80,8 +80,8 @@ window.addEventListener('load', () => {
     update(input, deltaTime, enemies) {
       // Collision detection
       enemies.forEach(enemy => {
-        const dx = enemy.x - this.x;
-        const dy = enemy.y - this.y;
+        const dx = (enemy.x + enemy.width / 2) - (this.x + this.width / 2);
+        const dy = (enemy.y + enemy.height / 2) - (this.y + this.height / 2);
         const distance = Math.sqrt(dx * dx + dy * dy);
         if (distance < enemy.width / 2 + this.width / 2) gameOver = true;
       })
